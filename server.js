57c489154fde54ed3f713fbb5aefc35345c42376
kunal05 <kunalsonne5@gmail.com>
2024-12-24 11:20:21 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,78 @@
+// Import dependenci
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('MongoDB connection failed:', err);
+});
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
+
+const User = mongoose.model('User', userSchema);
+
+
+
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'login.html'));
+});
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'home.html'));
+});
+
+app.post('/signup', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const newUser = new User({ email, password });
+        await newUser.save();
+        res.redirect('/home'); 
+    } catch (error) {
+        console.error('Error registering user:', error);
+        res.status(500).send('Error registering user');
+    }
+});
+
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) return res.status(404).send('User not found');
+
+        if (password === user.password) {
+            res.redirect('/home'); 
+        } else {
+            res.status(401).send('Invalid password');
+        }
+    } catch (error) {
+        console.error('Error logging in:', error);
+        res.status(500).send('Error logging in');
+    }
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
