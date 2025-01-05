13ddad8982caec9269fa0391ff78d5be539a2913
kunal05 <kunalsonne5@gmail.com>
2025-01-05 10:14:04 +0530

    Update Base.jsx

--- frontend/src/components/Base.jsx
+++ frontend/src/components/Base.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../styles/styles.css';
-// import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import { Link, useLocation } from 'react-router-dom';
 
